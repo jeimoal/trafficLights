2f34275969d8fe12fcf72bc66401a881a08facad
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-08-12 14:47:13 -0300

    Update tLights.js

--- tLights.js
+++ tLights.js
@@ -2,52 +2,66 @@ const lts = {
 timer : null,
 setTimer : 10,
 setCaut : 5,
+setStop : 10,
 
 init : () => {
-	trafficLights = lts.decrs();
+	lts.timer = setInterval(lts.tickGrn, 1000);
 },
 
-decrs : () => {
-	lts.timer = setInterval(lts.tick, 1000);
-},
+tickGrn : () => {
+count = lts.setTimer;
 
-tick : () => {
-	count = lts.setTimer;
-
-	if(count > 0) {
-		display = document.querySelector('#trfTimer');
-		display.textContent = 'go!';
-
-		gTxt = document.querySelector('#grnTxt');
-		gTxt.textContent = count;
-		lts.setTimer--;
-	} else {
-		gTxt.textContent = "";
-		clear = clearInterval(lts.tick);
-		lts.tickCaut();
-	};
-
-
+if(count > 0) {
+	display = document.querySelector('#trfTimer');
+	display.textContent = 'go!';
+	gTxt = document.querySelector('#grnTxt');
+	gTxt.textContent = count;
+	lts.setTimer--;
+} else {
+	gTxt.textContent = "";
+	clear = clearInterval(lts.tickGrn);
+	lts.tickCaut();
+	}
 },
 
 tickCaut : () => {
-	count = lts.setCaut;
+count = lts.setCaut;
 
-	if(count > 0) {
-		display = document.querySelector('#trfTimer');
-		display.textContent = 'caution';
+if(count > 0) {
+	display = document.querySelector('#trfTimer');
+	display.textContent = 'caution';
 
-		gTxt = document.querySelector('#ylwTxt');
-		gTxt.textContent = count;
-		lts.setCaut--;
-	} else {
-		gTxt.textContent = "";
-		clear = clearInterval(lts.tickCaut);
-		//lts.tickStop();
-	};
+	gTxt = document.querySelector('#ylwTxt');
+	gTxt.textContent = count;
+	lts.setCaut--;
+} else {
+	gTxt.textContent = "";
+	clear = clearInterval(lts.tickCaut);
+	lts.tickStop();
+	}
 },
 
-//end
+tickStop : () => {
+count = lts.setStop;
+
+if(count > 0) {
+	display = document.querySelector('#trfTimer');
+	display.textContent = 'stop';
+
+	gTxt = document.querySelector('#redTxt');
+	gTxt.textContent = count;
+	lts.setStop--;
+} else {
+	gTxt.textContent = "";
+	clear = clearInterval(lts.tickStop);
+	lts.setTimer = 10;
+	lts.setCaut = 5;
+	lts.setStop = 10;
+	lts.tickGrn();
+	}
+},
+
+
 };
 
 window.addEventListener('load', lts.init);
